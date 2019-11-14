e07904a84982c6326e02bfb21592d891352aa1ce
xnltu289 <xnltu289@gmail.com>
2019-11-14 20:05:04 +0900

    Aria Server File

---
+++ Aria_Server/winform real server/PI/Pi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winform_real_server.PI
+{
+    class Pi
+    {
+        public string Pi_lot_id { get; set; }
+        public string Pi_prod_model { get; set; }
+        public string Pi_prod_count { get; set; }
+        public string Pi_line { get; set; }
+        public string Pi_prod_color { get; set; }
+    }
+}
